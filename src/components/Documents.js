ac868e5040c517667b1db8642fff671f5d2878aa
Steffani <sartinisteffani@gmail.com>
2022-05-06 15:44:28 +0200

    favicon + attributes table for Documentation

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import { Link } from "react-scroll";
+import  CustomizedTables  from "./Attributes";
 
 const Documents = () => {
   return (
@@ -14,6 +15,16 @@ const Documents = () => {
               style={{ textDecoration: "none" }}
             >
               Introduction
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="currentVersion"
+              spy={true}
+              smooth={true}
+              style={{ textDecoration: "none" }}
+            >
+              Current Version
             </Link>
           </li>
           <li>
@@ -147,119 +158,47 @@ const Documents = () => {
           <div id="introduction">Introduction</div>
         </Typography>
         <Typography align="center">
-          Cake gummi bears brownie marshmallow powder tootsie roll macaroon
-          caramels chocolate. Ice cream croissant macaroon wafer powder. Donut
-          cotton candy lollipop carrot cake liquorice. Jujubes dessert oat cake
-          soufflé donut. Muffin lollipop cheesecake icing topping. Carrot cake
-          biscuit chocolate jelly chocolate bar. Donut marshmallow sugar plum
-          gingerbread apple pie toffee. Dessert lollipop cupcake candy caramels
-          cotton candy pie. Shortbread pie soufflé gummi bears toffee sesame
-          snaps wafer gummies donut. Sweet roll pudding croissant cheesecake
-          gummies sweet roll apple pie.
+        This documentation should give you all the information to make your HTTP
+        requests, making you familiar with what's available to you to develop your
+         project.
+        </Typography>
+        <Typography variant="h4" align="center" className="title">
+          <div id="currentVersion">Current Version</div>
+        </Typography>
+        <Typography>
+        The current version of the API is 1. The Game Characters API is an open 
+        API, which means no authentication is required to query the data.
+         Since no authentication is needed, there is only support for GET requests. 
         </Typography>
         <Typography variant="h4" align="center" className="title">
           <div id="rate">Rate limit</div>
         </Typography>
         <Typography>
-          Cupcake ipsum dolor sit amet bear claw jelly-o. Marzipan gummi bears
-          bear claw pie apple pie. Candy pastry chupa chups powder cake
-          chocolate. Toffee cupcake lemon drops toffee pastry danish dessert.
-          Jelly beans gingerbread jelly-o tart pudding. Marshmallow lollipop
-          donut bear claw donut. Jujubes dessert oat cake soufflé donut. Muffin
-          lollipop cheesecake icing topping. Carrot cake biscuit chocolate jelly
-          chocolate bar. Donut marshmallow sugar plum gingerbread apple pie
-          toffee. Dessert lollipop cupcake candy caramels cotton candy pie.
-          Shortbread pie soufflé gummi bears toffee sesame snaps wafer gummies
-          donut. Sweet roll pudding croissant cheesecake gummies sweet roll
-          apple pie. Cake tiramisu apple pie marshmallow caramels apple pie
-          fruitcake. Oat cake gummies ice cream oat cake chocolate cake cookie
-          shortbread bear claw tiramisu. Cupcake soufflé soufflé topping soufflé
-          pudding. Chupa chups apple pie cotton candy marzipan icing topping
-          icing pudding. Sugar plum gingerbread sweet carrot cake pie jelly-o.
-          Topping ice cream toffee cotton candy sugar plum cupcake. Sweet
-          jelly-o candy sweet roll ice cream halvah icing sweet roll.
+        There is a rate limit of 60 requests per hour from each IP address to prevent
+         harmful activity. If you reach that limit, you will receive a message and 
+         regain access after an hour.
         </Typography>
         <Typography variant="h4" align="center" className="title">
           <div id="base">Base URL</div>
         </Typography>
-        <Typography>
-          Cupcake ipsum dolor sit amet bear claw jelly-o. Marzipan gummi bears
-          bear claw pie apple pie. Candy pastry chupa chups powder cake
-          chocolate. Toffee cupcake lemon drops toffee pastry danish dessert.
-          Jelly beans gingerbread jelly-o tart pudding. Marshmallow lollipop
-          donut bear claw donut. Jujubes dessert oat cake soufflé donut. Muffin
-          lollipop cheesecake icing topping. Carrot cake biscuit chocolate jelly
-          chocolate bar. Donut marshmallow sugar plum gingerbread apple pie
-          toffee. Dessert lollipop cupcake candy caramels cotton candy pie.
-          Shortbread pie soufflé gummi bears toffee sesame snaps wafer gummies
-          donut. Sweet roll pudding croissant cheesecake gummies sweet roll
-          apple pie. Cake tiramisu apple pie marshmallow caramels apple pie
-          fruitcake. Oat cake gummies ice cream oat cake chocolate cake cookie
-          shortbread bear claw tiramisu. Cupcake soufflé soufflé topping soufflé
-          pudding. Chupa chups apple pie cotton candy marzipan icing topping
-          icing pudding. Sugar plum gingerbread sweet carrot cake pie jelly-o.
-          Topping ice cream toffee cotton candy sugar plum cupcake. Sweet
-          jelly-o candy sweet roll ice cream halvah icing sweet roll.
+        <Typography align="center" className="routesURL">
+        https://gamecharactersapi.herokuapp.com/playground   
         </Typography>
         <Typography variant="h3" align="center" className="title">
           <div id="characters">Characters</div>
         </Typography>
         <Typography>
-          Jelly danish chocolate chocolate oat cake cake candy. Sweet cookie
-          toffee lemon drops marzipan lollipop. Danish cake sugar plum cake oat
-          cake gummi bears chocolate lemon drops gummies. Marzipan cake candy
-          canes soufflé gummi bears gummi bears brownie oat cake candy. Pastry
-          chocolate bar topping candy canes wafer chocolate cake croissant
-          gummies jelly-o. Apple pie liquorice muffin apple pie pudding
-          fruitcake. Dessert biscuit chocolate cake lemon drops pastry apple pie
-          jelly beans sweet jelly-o. Jelly beans chocolate cake dragée cake
-          carrot cake. Tiramisu dessert pudding pudding bear claw. Dessert
-          danish cotton candy chocolate bar oat cake oat cake. Cake gingerbread
-          chocolate cake sweet carrot cake chupa chups jelly wafer croissant.
-          Halvah cake carrot cake jelly beans chocolate bar danish toffee wafer
-          jelly. Biscuit cake lemon drops danish carrot cake sesame snaps pastry
-          candy canes. Apple pie sweet roll danish ice cream cake cake cake
-          dessert halvah. Jujubes carrot cake sweet lollipop sweet cake bonbon
-          topping. Topping cupcake icing jujubes macaroon. Biscuit powder
-          jujubes wafer tootsie roll chocolate cake. Oat cake liquorice cake
-          croissant oat cake chocolate fruitcake cheesecake. Lemon drops sweet
-          marzipan ice cream biscuit chocolate cake biscuit. Gummi bears sweet
-          biscuit gummies jelly-o toffee macaroon pie apple pie. Fruitcake chupa
-          chups fruitcake gingerbread oat cake lollipop lemon drops croissant
-          bear claw. Shortbread apple pie pudding macaroon cookie liquorice
-          dessert. Carrot cake cupcake sesame snaps cake macaroon sweet roll
-          toffee chocolate cake sesame snaps. Cake jelly-o sesame snaps jujubes
-          marzipan marzipan cheesecake. Topping tiramisu muffin jujubes
-          chocolate bar lemon drops caramels shortbread toffee.
+          The database structure is inspired by the Final Fantasy
+           games and all the amazing characters through out the series.
+           The intention is to continue enlarging the database gradualy
+           with new characters. If you are a big fan of the series and
+           want to contribure to this project feel free to contact us.
         </Typography>
         <Typography variant="h4" align="center" className="title">
-          <div id="endpoints">Character endpoint</div>
+          <div id="endpoints">Characters endpoints</div>
         </Typography>
         <Typography>
-          Lollipop cupcake cotton candy croissant gingerbread. Soufflé jelly-o
-          apple pie wafer tiramisu marzipan. Cake tart jelly-o danish dragée
-          muffin. Cookie cookie bonbon chocolate sugar plum toffee icing.
-          Brownie carrot cake bonbon sesame snaps jujubes wafer brownie.
-          Lollipop croissant cheesecake gingerbread cheesecake soufflé tart
-          chocolate fruitcake. Dessert bear claw candy canes oat cake jujubes
-          cake candy sweet chupa chups. Icing pudding cupcake gingerbread toffee
-          dessert. Pudding bear claw muffin marshmallow muffin oat cake.
-          Marshmallow shortbread wafer marshmallow icing toffee. Candy canes
-          caramels halvah chupa chups macaroon gummi bears jelly-o sugar plum.
-          Donut pudding donut bear claw fruitcake marzipan powder bear claw ice
-          cream. Lemon drops toffee candy cake marshmallow. Gummi bears danish
-          jelly beans tootsie roll carrot cake apple pie soufflé pie caramels.
-          Marshmallow soufflé shortbread bear claw sweet cotton candy. Sesame
-          snaps macaroon toffee dragée apple pie cotton candy macaroon topping.
-          Sweet roll croissant sesame snaps jelly-o marshmallow cookie jelly-o.
-          Toffee sweet sugar plum cookie cookie jujubes danish macaroon.
-          Cheesecake ice cream danish ice cream jelly-o lemon drops candy canes
-          apple pie. Chupa chups jelly-o sesame snaps sweet roll halvah toffee.
-          Cotton candy sweet roll pie sesame snaps wafer. Topping marzipan
-          brownie powder pie caramels halvah jujubes. Shortbread caramels danish
-          donut oat cake soufflé. Sugar plum gummies chupa chups dragée candy
-          pudding. Apple pie shortbread croissant gummies cheesecake topping
-          gingerbread carrot cake icing.
+           <CustomizedTables/>
         </Typography>
         <Typography variant="h4" align="center" className="title">
           <div id="get-all">Get all characters</div>
@@ -482,3 +421,5 @@ const Documents = () => {
 };
 
 export default Documents;
+
+
