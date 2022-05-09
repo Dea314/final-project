b58e5d3e15212cf04b200f282f3b7050a3478c13
Dea314 <fabrisdanijela@gmail.com>
2022-05-09 17:37:19 +0200

    button styling

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -3,7 +3,22 @@ import useCopyToClipboard from "../hooks/useCopyToClipboard";
 
 const CopyButton = ({ textToCopy }) => {
   const [isCopied, handleCopy] = useCopyToClipboard();
-  return <button onClick={() => handleCopy(textToCopy)}>Copy</button>;
+  return (
+    <button
+      style={{
+        backgroundColor: "#5279EC",
+        color: "#fff",
+        padding: "1rem",
+        borderRadius: "5px",
+        fontSize: "1rem",
+        border: "1px solid white",
+        marginLeft: "3rem",
+      }}
+      onClick={() => handleCopy(textToCopy)}
+    >
+      Copy to clipboard
+    </button>
+  );
 };
 
 export default CopyButton;
