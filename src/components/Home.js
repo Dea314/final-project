f24f1cd8e8dd297af8b3c2b43b860717ade5161e
Rabs <rabbolinif@gmail.com>
2022-05-10 18:17:24 +0200

    Introduction added to home page - first draft

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,18 @@ const Home = ({ response }) => {
 
   return (
     <>
+      <div className="home_intro_h1">
+        <h1>The RESTful Game Characters API</h1>
+      </div>
+      <div className="home_intro_h2">
+        <p>All the data of the characters from Final Fantasy
+          <br></br>
+          at your fingure tips accessible through our RESTful API</p>
+          <p>
+            Check out the <a href="/Documents">documentation</a> or try out our <a href="/Playground">playground</a> now!
+          </p>
+
+      </div>
       <div className="home">
         {response.map((item) => {
           return <ActionAreaCard character={item} />;
