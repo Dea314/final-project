1cf0f3980f6f78edc868711d8859b37134b25c37
Steffani <sartinisteffani@gmail.com>
2022-05-11 15:00:33 +0200

    fixing card sizes from home

--- src/components/ActionAreaCard.js
+++ src/components/ActionAreaCard.js
@@ -11,8 +11,8 @@ const assets = require("../assets.json");
 const ActionAreaCard = ({ character }) => {
   return (
     <>
-      <Card sx={{ maxWidth: 350 }}>
-        <CardActionArea>
+      <Card sx={{ width: "20%" }}>
+        <CardActionArea sx={{ height: "100%" }}>
           <CardMedia
             component="img"
             height="350"
@@ -25,7 +25,7 @@ const ActionAreaCard = ({ character }) => {
               boxShadow: "0 2rem 6rem rgba(0, 0, 0, 0.1)",
             }}
           />
-          <CardContent>
+          <CardContent sx={{ height: "100%" }}>
             <Typography gutterBottom variant="h5" color="#5279EC">
               {character.name}
             </Typography>
